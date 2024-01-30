f86bc56149080517a8645410ab0761737447be64
imranahamed1909 <imranahamed1909@gmail.com>
2024-01-30 19:11:52 +0600

    added live message

--- pages/connecting.js
+++ pages/connecting.js
@@ -1,11 +1,39 @@
 import Webcam from "react-webcam";
+import Pusher from "pusher-js";
+import { useState, useEffect } from "react";
 
 export default function ConnectingPage() {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    const pusher = new Pusher(
+      "a5f0008dea3736f30a17", // APP_KEY
+      {
+        cluster: "ap2",
+        encrypted: true,
+      }
+    );
+
+    const channelName = "userChat";
+
+    const channel = pusher.subscribe(channelName);
+    channel.bind("chat-notification", (data) => {
+      setMessage(data.text);
+      // console.log("message:", data);
+    });
+
+    return () => {
+      channel.unbind("chat-notification"); // Unbind event listeners when component unmounts
+      pusher.unsubscribe(channelName);
+    };
+  }, []);
+
   return (
     <div className="relative text-black h-screen w-screen flex flex-col justify-center items-center">
-      <h1 className="absolute top-[40px] lg:top-[140px] text-black font-bold text-[30px]">
-        Connecting...
-      </h1>
+      <div className="absolute top-[40px] lg:top-[140px] text-black">
+        <h1 className="font-bold text-3xl">Connecting...</h1>
+        {message && <p className="mt-10 text-2xl font-medium">{message}</p>}
+      </div>
       <Webcam
         audio={false}
         className="object-cover h-screen w-screen lg:w-auto"
